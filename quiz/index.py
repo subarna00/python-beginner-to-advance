fcbe3aef7e6d320716ca05b084dbf7b081af9841
subarna <subarna@subarnas-MacBook-Pro.local>
2024-03-17 10:28:56 +0545

    quiz

---
+++ quiz/index.py
@@ -0,0 +1,13 @@
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Lets play! ")
+ 
+answer = input("What is the full form of CPU? ")
+
+if answer == 'central processing unit':
+    print("correct")
+else:
+    print("Incorrect")
